eddfc5d70b7e3232baca2cd9855f6a6d8b85014d
Alexander <ghazaryan.alexander@gmail.com>
2020-10-16 22:04:43 +0400

    Update Learning.py

--- Learning.py
+++ Learning.py
@@ -1,4 +1,8 @@
-import pygame
+try:
+	import pygame
+except:
+	from os import system
+	system("pip install pygame")
 from pyautogui import size
 def is_collision(x_1, y_1, width_1, height_1, x_2, y_2, width_2, height_2):
 	return x_1 in range(x_2 + 1 - width_1, x_2 - 1 + width_2) and y_1 in range(y_2 + 1 - height_1, y_2 - 1 + height_2)
